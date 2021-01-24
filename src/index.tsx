188349de7ff6ac936db7c00e76de2cba5ecc87c9
Eldar Abdullazyanov <eldar.abdullazyanov@akvelon.com>
2021-01-24 18:58:23 +0300

    feat: added the implementation

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type SmsUserConsentType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { SmsUserConsent } = NativeModules;
-
-export default SmsUserConsent as SmsUserConsentType;
